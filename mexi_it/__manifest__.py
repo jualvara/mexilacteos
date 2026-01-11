8e4fcc95785099811c068ab7c161a477b035df8e
Juan Carlos Alvarado <jualvara@ucm.es>
2026-01-11 01:43:36 +0100

    feat: initial module structure, manifest and sequence configuration

---
+++ mexi_it/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Mexilacteos IT",
+    "summary": "IT request module for Mexilacteos",
+    "version": "18.0.1.0.0",
+    "category": "Services",
+    "author": "Mexilacteos",
+    "license": "Other proprietary",
+    "application": True,
+    "depends": ["base", "mail", "hr", "maintenance"],
+    "data": ["data/it_request_sequence.xml"],
+    "test": [],
+}
